6400492a1e69653c98eb1878670b6b3803753211
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2017-06-11 01:59:21 +0700

    debug order response

--- res/order_res.py
+++ res/order_res.py
@@ -12,7 +12,7 @@ class OrderRes(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument(name="items", type=list, location = "json")
+        parser.add_argument(name="items", type=list ,location = "json")
         parser.add_argument(name="user_id", type=str, location="json")
         parser.add_argument(name="id", type=str, location="json")
         parser.add_argument(name="count", type=int, location="json")
@@ -22,16 +22,15 @@ class OrderRes(Resource):
         user_id = body.user_id
         total_spend = 0
         order_item = []
-        for item in items:
-             print("1")
+        # for item in items:
         #    good_id = item["id"]
-        #     count = item["count"]
-        #     good = Good.objects().with_id(good_id)
-        #     price = good.price
-        #     print("good_id:", good_id,";count: ",count,"price: ",price)
-        #     total_spend += price*count
-        #     singleOrder = SingleOrder(good = good, count = count)
-        #     order_item.append(singleOrder)
+        #    count = item["count"]
+        #    good = Good.objects().with_id(good_id)
+        #    price = good.price
+        #    print("good_id:", good_id,";count: ",count,"price: ",price)
+        #    total_spend += price*count
+        #    singleOrder = SingleOrder(good = good, count = count)
+        #    order_item.append(singleOrder)
         # customer = Customer.objects().with_id(user_id)
         # print(mlab.item2json(order_item[0]))
         # print("order_item0:",mlab.item2json(order_item[0]),"order_item1:",order_item[1])
@@ -39,8 +38,8 @@ class OrderRes(Resource):
         #               totalspend = total_spend)
         # order.save()
         # add_order = Order.objects().with_id(order.id)
-        #return mlab.item2json(add_order)
-        return {"message":"ok"},200
+        return items
+
 
 
 
