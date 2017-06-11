7f806d4f6771e224582d52abfd8c2bda1de99050
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2017-06-11 11:30:24 +0700

    back up ban goc

--- res/order_res.py
+++ res/order_res.py
@@ -12,41 +12,33 @@ class OrderRes(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument(name="items", type=list ,location="json")
+        parser.add_argument(name="items", type=list, location="json")
         parser.add_argument(name="user_id", type=str, location="json")
         parser.add_argument(name="id", type=str, location="form")
         parser.add_argument(name="count", type=int, location="form")
 
         body = parser.parse_args()
         items = body["items"]
-        # user_id = body.user_id
-        # total_spend = 0
-        # order_item = []
+        user_id = body.user_id
+        total_spend = 0
+        order_item = []
         for item in items:
-            good_id = item["id"]
-        #    count = item["count"]
-        #    good = Good.objects().with_id(good_id)
-        #    price = good.price
-        #    print("good_id:", good_id,";count: ",count,"price: ",price)
-        #    total_spend += price*count
-        #    singleOrder = SingleOrder(good = good, count = count)
-        #    order_item.append(singleOrder)
-        # customer = Customer.objects().with_id(user_id)
-        # print(mlab.item2json(order_item[0]))
-        # print("order_item0:",mlab.item2json(order_item[0]),"order_item1:",order_item[1])
-        # order = Order(items = order_item,customer = customer,
-        #               totalspend = total_spend)
-        # order.save()
-        # add_order = Order.objects().with_id(order.id)
-        # if isinstance(items,dict):
-        #   type = "dict"
-        # else:
-        #     if isinstance(items,list):
-        #         type = "list"
-        #     else:
-        #         if isinstance(items,tuple):
-        #             type = "tuple"
-        return good_id
+           good_id = item["id"]
+           count = item["count"]
+           good = Good.objects().with_id(good_id)
+           price = good.price
+           print("good_id:", good_id,";count: ",count,"price: ",price)
+           total_spend += price*count
+           singleOrder = SingleOrder(good = good, count = count)
+           order_item.append(singleOrder)
+        customer = Customer.objects().with_id(user_id)
+        print(mlab.item2json(order_item[0]))
+        print("order_item0:",mlab.item2json(order_item[0]),"order_item1:",order_item[1])
+        order = Order(items = order_item,customer = customer,
+                      totalspend = total_spend)
+        order.save()
+        add_order = Order.objects().with_id(order.id)
+        return mlab.item2json(add_order)
 
 
 
